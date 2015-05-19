d8f268b860bce478c4c48b8887c28662c9283d54
Vinicius Reis <luiz.vinicius73@gmail.com>
2015-05-18 22:22:38 -0300

    init

---
+++ src/Providers/WarehouseProvider.php
@@ -0,0 +1,17 @@
+<?php namespace Artesaos\Warehouse\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class WarehouseProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+}
--- src/resources/lang/en/criteria.php
+++
@@ -1,4 +0,0 @@
-<?php
-return array(
-    'fields_not_accepted'=>'Columns :field are not accepted in the research'
-);
